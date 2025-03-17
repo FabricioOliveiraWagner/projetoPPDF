f72a6cfafb6b5fefc666a6e1447a0421d49e2c43
Fabricio Wagner <fabricio.wagner@qintess.com>
2025-03-17 20:34:45 -0300

    Comit inicial do projeto

---
+++ recrutamento/src/test/java/br/com/ppdf/recrutamento/RecrutamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ppdf.recrutamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
